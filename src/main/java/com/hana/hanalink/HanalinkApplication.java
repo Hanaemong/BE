ff01ad58d645070a29861b5d8ed95825aa39e439
khw18 <gmsb14@naver.com>
2024-06-21 12:59:58 +0900

    Feat: init

---
+++ src/main/java/com/hana/hanalink/HanalinkApplication.java
@@ -0,0 +1,13 @@
+package com.hana.hanalink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanalinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanalinkApplication.class, args);
+    }
+
+}
