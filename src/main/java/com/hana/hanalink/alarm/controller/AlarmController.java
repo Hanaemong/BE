f039930e01786b35753ffb67b2e2fedd9fceaf5e
안나영 <dpswpfskdud@naver.com>
2024-06-26 10:58:30 +0900

    Feat: 알림리스트 api 구현 #5

--- src/main/java/com/hana/hanalink/alarm/controller/AlarmController.java
+++ src/main/java/com/hana/hanalink/alarm/controller/AlarmController.java
@@ -3,8 +3,9 @@ package com.hana.hanalink.alarm.controller;
 
 import com.hana.hanalink.alarm.dto.response.AlarmRes;
 import com.hana.hanalink.alarm.service.AlarmService;
-import com.hana.hanalink.member.domain.Member;
+import com.hana.hanalink.member.domain.MemberDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,8 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     @GetMapping("/alarm")
-    public List<AlarmRes> getAlarmList(){
-        return alarmService.getAlarmList(new Member().getMemberId());
+    public List<AlarmRes> getAlarmList(@AuthenticationPrincipal MemberDetails member){
+        return alarmService.getAlarmList(member.getMemberId());
     }
-
 
 }
