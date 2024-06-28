c3b5e767242c6170fc5541abd84765e82446ed15
안나영 <dpswpfskdud@naver.com>
2024-06-28 11:18:39 +0900

    Feat: S3 set & Plan imageUpload #31

--- src/main/java/com/hana/hanalink/plan/dto/PlanPostReq.java
+++ src/main/java/com/hana/hanalink/plan/dto/PlanPostReq.java
@@ -5,15 +5,15 @@ import com.hana.hanalink.team.domain.Team;
 
 import java.time.LocalDateTime;
 
-public record PlanPostReq (String planName, LocalDateTime planDate, String planPlace, long planCost, String planImg ){
+public record PlanPostReq (String planName, LocalDateTime planDate, String planPlace, long planCost ){
 
-    public Plan toEntity(Team team){
+    public Plan toEntity(Team team,String image){
         return Plan.builder()
                 .planName(planName)
                 .place(planPlace)
                 .planDate(planDate)
                 .cost(planCost)
-                .planImg(planImg)
+                .planImg(image)
                 .team(team)
                 .build();
     }
