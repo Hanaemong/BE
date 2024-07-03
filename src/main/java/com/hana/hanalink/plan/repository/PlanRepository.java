4e28d8b417ed3bf0539d40076cc189f3d3d70ad1
khw18 <gmsb14@naver.com>
2024-07-03 15:56:29 +0900

    Feat: get plans #73

--- src/main/java/com/hana/hanalink/plan/repository/PlanRepository.java
+++ src/main/java/com/hana/hanalink/plan/repository/PlanRepository.java
@@ -1,9 +1,11 @@
 package com.hana.hanalink.plan.repository;
 
 import com.hana.hanalink.plan.domain.Plan;
+import com.hana.hanalink.team.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlanRepository extends JpaRepository<Plan,Long> {
-
-
+    List<Plan> findByTeam(Team team);
 }
