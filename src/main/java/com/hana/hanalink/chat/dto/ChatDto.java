97125e5cdfa72a07d985448dbdaabd35da57d6ae
khw18 <gmsb14@naver.com>
2024-07-08 10:56:03 +0900

    Feat: chat + profile #109

--- src/main/java/com/hana/hanalink/chat/dto/ChatDto.java
+++ src/main/java/com/hana/hanalink/chat/dto/ChatDto.java
@@ -13,28 +13,17 @@ import lombok.NoArgsConstructor;
 public class ChatDto {
     private String id;
     private String roomId;
-    private String type;
+    private String profile;
     private String nickname;
     private String msg;
     private String time;
     public Chat toEntity() {
         return Chat.builder()
                 .roomId(roomId)
-                .type(type)
+                .profile(profile)
                 .nickname(nickname)
                 .msg(msg)
                 .time(time)
                 .build();
-    }
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public void newConnect() {
-        this.type = "new";
-    }
-
-    public void closeConnect() {
-        this.type = "close";
     }
 }
