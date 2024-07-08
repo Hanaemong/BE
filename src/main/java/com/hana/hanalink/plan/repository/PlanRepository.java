964e8edeea939a54a6f98b1ca96745878a0e3d52
khw18 <gmsb14@naver.com>
2024-07-08 15:24:24 +0900

    Fix: plandate desc

--- src/main/java/com/hana/hanalink/plan/repository/PlanRepository.java
+++ src/main/java/com/hana/hanalink/plan/repository/PlanRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan,Long> {
-    List<Plan> findByTeamOrderByCreatedAtDesc(Team team);
+    List<Plan> findByTeamOrderByPlanDateDesc(Team team);
 }
--- src/main/java/com/hana/hanalink/plan/service/PlanService.java
+++ src/main/java/com/hana/hanalink/plan/service/PlanService.java
@@ -41,7 +41,7 @@ public class PlanService {
 
     public List<PlanRes> getPlans(Long teamId) {
         Team team = teamRepository.findById(teamId).orElseThrow(TeamNotFoundException::new);
-        List<Plan> planList = planRepository.findByTeamOrderByCreatedAtDesc(team);
+        List<Plan> planList = planRepository.findByTeamOrderByPlanDateDesc(team);
 
         return planList.stream()
                 .map(this::buildPlanRes)
